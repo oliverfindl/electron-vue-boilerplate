c3a80e004434b799bc0098b58f5d9fce3d4f3c03
Oliver Findl <github@findl.sk>
2018-05-19 21:39:52 +0200

    Some minor changes to Vue app

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="app">
 		<img src="./assets/logo.png" />
-		<h1>{{ msg }}</h1>
+		<h1>{{ message }}</h1>
 		<h2>Essential Links</h2>
 		<ul>
 			<li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
@@ -21,12 +21,10 @@
 
 <script>
 	export default {
-		name: "app",
-		data() {
-			return {
-				msg: "Welcome to Your Vue.js App"
-			}
-		}
+		name: "App",
+		data: () => ({
+			message: "Welcome to Your Vue.js App"
+		})
 	}
 </script>
 
