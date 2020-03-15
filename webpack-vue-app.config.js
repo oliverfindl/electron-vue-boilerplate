2bf184e0cdae9b545f41a76ddc1a13c73a223790
Oliver Findl <github@findl.sk>
2020-03-15 20:10:25 +0100

    [revert] Removed Webpack Bundle Analyzer plugin

--- webpack-vue-app.config.js
+++ webpack-vue-app.config.js
@@ -2,7 +2,6 @@
 
 const { resolve } = require("path");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -156,7 +155,6 @@ module.exports = {
 	},
 	plugins: [
 		new VueLoaderPlugin(),
-		new BundleAnalyzerPlugin(),
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			filename: "index.html",
