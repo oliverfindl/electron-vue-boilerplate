a0fdf1d78b21164452c0e98c575740b1e1a984e5
Oliver Findl <github@findl.sk>
2020-07-08 20:52:35 +0200

    Updated postcss config file

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,11 @@ module.exports = {
 	plugins: [
 		purgecss({
 			content: ["src/vue-app/index.html", "src/vue-app/**/*.js", "src/vue-app/**/*.vue"],
+			defaultExtractor(content) {
+				const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, "");
+				return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+			},
+			whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/],
 			keyframes: true,
 			fontFace: true,
 			variables: true
