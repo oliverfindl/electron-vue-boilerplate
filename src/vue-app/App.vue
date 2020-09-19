47a595ad68a2ae32f36cf6cec2de26f3271d2eec
Oliver Findl <github@findl.sk>
2020-09-19 21:45:53 +0200

    Updated App

--- src/vue-app/App.vue
+++ src/vue-app/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="app">
-		<img alt="Vue logo" class="vue-logo" src="@/assets/logo.svg" />
+		<img class="vue-logo" src="@/assets/logo.svg" alt="Vue logo" />
 		<HelloWorld message="Welcome to Your Vue.js App" />
 	</div>
 </template>
