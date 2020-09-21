9ca54471d4de596925fcbb5c90bd7995e84c1002
Oliver Findl <github@findl.sk>
2020-09-21 12:22:57 +0200

    Updated dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -6,12 +6,12 @@ module.exports = {
 	syntax: "postcss-scss",
 	plugins: [
 		purgecss({
-			content: ["src/vue-app/index.html", "src/vue-app/**/*.js", "src/vue-app/**/*.vue"],
+			content: [ "src/vue-app/index.html", "src/vue-app/**/*.js", "src/vue-app/**/*.vue" ],
 			defaultExtractor(content) {
 				const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, "");
 				return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
 			},
-			whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/],
+			safelist: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/ ],
 			keyframes: true,
 			fontFace: true,
 			variables: true
